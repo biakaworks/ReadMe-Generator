474aec094ad1ac9fe4972d5b79350e1a8a7b2717
Biaka Z <biaka@biakaworks.com>
2020-11-11 22:24:45 -0600

    Updated Index.js with MD requirements

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
-console.log("Yo Test Test")
+console.log("Yo Test Test");
 
 // Const that RMG needs to have in the beginning
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 // const readmeGenerator = require('./utils/readmeGenerator')
 
 // The questions that ReadMe_Generator will ask the user
 inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "projectTitle",
-            message: "What is the title of your Project?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Please give a short description of your Project",
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "How do you install your Project?",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Please describe how to use your Project.",
-        },
-        {
-            type: "input",
-            name: "contributors",
-            message: "Who are the contributers in this Project?",
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "What is your ReadMe's Title?",
-        },
-    ])
-    .then((data) => {
-        const filename = `${data.projectTitle.toLowerCase().split(' ').join('')}.md`;
-        let template =`# ${data.projectTitle}
+  .prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your Project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please give a short description of your Project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How do you install your Project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please describe how to use your Project.",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Who are the contributers in this Project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What is your ReadMe's Title?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your contact email",
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "Please add your GitHub username",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.projectTitle
+      .toLowerCase()
+      .split(" ")
+      .join("")}.md`;
+    let template = `<h1 align="center">${data.projectTitle}</h1>
+
 ## Description
+📖 ${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Email](#email)
+- [GitHub](#gitHub)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💿 ${data.installation}
+
+## Email
+📧 ${data.email}
+
+## Github
+📧 ${data.gitHub}
 
 This project solves the security concern of weak passwords. Given that a user needs a new, secured password, they will be promted to determine certain character types required for the password and then a new random password will be generated.
 
@@ -57,18 +90,11 @@ The criteria that the password is able to meet are: <br>
 2) the password must have at least one of the following character types: <br>
     *lowercase characters <br>
     *uppercasse characters <br>
-    *special characters <br>
-    *numeric characters <br>
+    *special characters <br>`;
 
-## The final product
- https://timothy-wesley.github.io/password-generator/
-
-<img src="example1.png" alt="picture of password generator"> <br>
-<img src="example2.png" alt="picture of completed password"> <br>
-
-        `
-    
-        fs.writeFile(filename, template, (err) =>
-          err ? console.log(err) : console.log('Success! Thanks for using ReadMe_Generator')
-        );
-      });
+    fs.writeFile(filename, template, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Success! Thanks for using ReadMe_Generator")
+    );
+  });
--- yolo.md
+++ yolo.md
@@ -1,7 +1,29 @@
-# vd
+<h1 align="center">yolo</h1>
+
 ## Description
+📖 yolo
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Email](#email)
+- [GitHub](#gitHub)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💿 yolo
+
+## Email
+📧 yolo
+
+## Github
+📧 yolo
 
 This project solves the security concern of weak passwords. Given that a user needs a new, secured password, they will be promted to determine certain character types required for the password and then a new random password will be generated.
+
+![Image of Version](https://img.shields.io/badge/ReadMeGen-v1.0-green)
 
 The new password implements greater security to sensitive data and saves time for users.
 
@@ -13,12 +35,3 @@ The criteria that the password is able to meet are: <br>
     *lowercase characters <br>
     *uppercasse characters <br>
     *special characters <br>
-    *numeric characters <br>
-
-## The final product
- https://timothy-wesley.github.io/password-generator/
-
-<img src="example1.png" alt="picture of password generator"> <br>
-<img src="example2.png" alt="picture of completed password"> <br>
-
-        
